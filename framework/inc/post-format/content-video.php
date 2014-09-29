187e1b16032e30cc57bd87fbf5dffa2753a7487f
Masato Takahashi <info@takahashimasato.com>
2014-09-29 23:59:38 +0900

    Update content-video.php v1.0.0

---
+++ framework/inc/post-format/content-video.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Blog Page Video Content
+ *
+ *
+ * @file           content-video.php
+ * @package        editit
+ * @author         Masato Takahashi
+ * @copyright      2014 wwwonder
+ * @version        Release: 1.0.0
+ * @filesource     wp-content/themes/editit/framework/inc/post-format/content-video.php
+ */
+?>
+<article class="post clearfix">
+
+  <?php if (rwmb_meta('editit_textareavideoembed') != "" ) : ?>
+  <div class="post-video embed-video">
+    <?php echo rwmb_meta('editit_textareavideoembed'); ?>
+  </div>
+  <?php elseif ( has_post_thumbnail() ) : ?>
+  <div class="post-image">
+    <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__('Permalink to %s', 'editit'), the_title_attribute('echo=0') ); ?>" class="link" rel="bookmark">
+      <?php the_post_thumbnail(); ?>
+    </a>
+  </div>
+  <?php endif; ?>
+
+  <div class="post-content">
+    <div class="post-content-header">
+      <div class="post-title">
+        <h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__('Permalink to %s', 'editit'), the_title_attribute('echo=0') ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+      </div>
+      <div class="post-meta">
+        <span class="meta-date"><time datetime="<?php echo date(DATE_W3C); ?>" class="updated"><?php the_time(get_option('date_format')); ?></time></span>
+        <span class="sep">|</span>
+        <span class="meta-author">By <a href="<?php echo get_author_posts_url(get_the_author_meta( 'ID' )); ?>" title="<?php printf( esc_attr__('View all posts by %s', 'editit'), get_the_author() ); ?>"><?php the_author(); ?></a></span>
+        <span class="sep">|</span>
+        <span class="meta-category"><?php the_category(', '); ?></span>
+        <?php if ( comments_open() ) : ?>
+        <span class="sep">|</span>
+        <span class="meta-comment"><?php comments_popup_link(__('No Comments', 'editit'), __('1 Comment', 'editit'), __('% Comments', 'editit'), 'comments-link', ''); ?></span>
+        <?php endif; ?>
+      </div>
+    </div>
+    <div class="post-excerpt">
+      <?php echo get_the_excerpt(); ?>
+      <div class="clear"></div>
+    </div>
+  </div>
+  
+</article>
