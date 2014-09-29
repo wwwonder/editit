8ff90113a44db7c42cea92d3763a774c4e8f6368
Masato Takahashi <info@takahashimasato.com>
2014-09-29 23:47:02 +0900

    Update nav.php v1.0.0

---
+++ framework/inc/nav.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Navigation
+ *
+ *
+ * @file           nav.php
+ * @package        editit
+ * @author         Masato Takahashi
+ * @copyright      2014 wwwonder
+ * @version        Release: 1.0.0
+ * @filesource     wp-content/themes/editit/framework/inc/nav.php
+ */
+?>
+<div id="pagination" class="pagination clearfix">
+  <?php editit_pagination(); ?>
+</div><!-- end of .pagination -->
